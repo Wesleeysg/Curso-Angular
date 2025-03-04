55d2035fb3e052358fbcb8f1a1ec105dbd246af0
Wesleeysg <wesleeysg@gmail.com>
2025-03-04 20:06:08 -0300

    -Feat: Inicial commit

---
+++ src/app/Components/user/users-dummy.ts
@@ -0,0 +1,33 @@
+export const USERS_DUMMY = [
+  {
+    id:'u1',
+    name: 'Wesley santos',
+    avatar:'user-1.jpeg'
+  },
+  {
+    id: 'u2',
+    name: 'Emily Thompson',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Marcus Johnson',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'David Miller',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Priya Patel',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Arjun Singh',
+    avatar: 'user-6.jpg',
+  },
+
+]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './Components/header/header.component'
+import { UserComponent } from "./Components/user/user.component";
+import { USERS_DUMMY } from './Components/user/users-dummy';
+import { TasksComponent } from "./Components/tasks/tasks.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Curso-Angular';
+  users = USERS_DUMMY;
+
+  selectuserId?: string;
+
+  get selectedUser(){
+    return this.users.find((user)=>user.id ===this.selectuserId);
+  }
+  OnSelectUser(id: string){
+    this.selectuserId = id;
+  }
 }
